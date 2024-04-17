d552ce5f1a82b98e9141f231da7c621cb41e9636
IbrahimDev04 <tu7nluy9f@code.edu.az>
2024-04-17 15:47:44 +0400

    first commit

---
+++ 16_Indexer/Models/Number.cs
@@ -0,0 +1,86 @@
+﻿namespace _16_Indexer.Models
+{
+    internal class Number
+    {
+        public int[] arr;
+        public int Capasity { get; set; } = 5;
+        public int Count { get; set; }
+        public Number()
+        {
+            arr = new int[Capasity];
+        }
+
+        public int this[int id]
+        {
+            get
+            {
+                if (id >= Count)
+                {
+                    throw new IndexOutOfRangeException("Arrayda bu parametrde deyer yoxdur");
+                }
+                return arr[id];
+            }
+            set
+            { 
+                arr[id] = value;
+            }
+        }
+
+        public void Add(int value)
+        {
+            if(Count == arr.Length)
+            {
+                Array.Resize(ref arr, Count + Capasity);
+            }
+            arr[Count] = value;
+            Count++;
+        }
+
+        public void RemoveAt(int id)
+        {
+            int[] newArr = new int[arr.Length];
+            int j = 0;
+            for (int i = 0 ; i < Count; i++)
+            {
+                if (i != id)
+                {
+                    newArr[j] = arr[i];
+                    j++;
+                }
+            }
+            arr = newArr;
+        }
+
+        public void IndexOf(int value)
+        {
+            for (int i = 0 ; i < Count;i++)
+            {
+                if (arr[i] == value)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+        }
+
+        public void LastIndexOf(int value)
+        {
+            for (int i = Count; i > 0; i--)
+            {
+                if (arr[i] == value)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+        }
+
+        public void GetElement()
+        {
+            for (int i = 0 ;i < Count;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ 16_Indexer/Program.cs
@@ -0,0 +1,25 @@
+﻿using _16_Indexer.Models;
+
+namespace _16_Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Number list = new Number();
+            list.Add(1);
+            list.Add(3);
+            list.Add(4);
+            list.Add(6);
+            list.Add(7);
+            list.Add(4);
+            list.Add(68);
+
+            list.GetElement();
+            list.IndexOf(4);
+            list.LastIndexOf(4);
+            Console.WriteLine(list[8]);
+
+        }
+    }
+}
